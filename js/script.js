6aa256b84ffb4f9a00ab2f0969bd81999b517b32
Maria Eduarda Araújo <104032250+MEduardaPSA@users.noreply.github.com>
2022-11-05 14:12:53 -0300

    finalizando projeto

--- js/script.js
+++ js/script.js
@@ -7,9 +7,12 @@ function createGame(playerOne, hour, playerTwo){
     </li>`
 }
 
+
+let delay = -0.4;
 function createCard(date, day, games){
+    delay += 0.4;
     return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
         <h2 class="card-date">${date} <span>${day}</span></h2>
         <ul>
             ${games}
@@ -17,28 +20,20 @@ function createCard(date, day, games){
     </div> `
 }
 
-document.querySelector("#app").innerHTML = `
-    <header class="header">
-        <img src="./images/logo.svg" alt="Logo NLW Copa">
-    </header>
-    <main id="cards">
+document.querySelector("#cards").innerHTML =
 
-        ${createCard('24/11', 'Quinta',
+        createCard('24/11', 'Quinta',
         createGame('uruguay-flag', '10:00', 'south-korea-flag') + 
         createGame('portugal-flag', '13:00', 'ghana-flag') +
-        createGame('brazil-flag', '16:00', 'serbia-flag')
-        )}
+        createGame('brazil-flag', '16:00', 'serbia-flag')) +
 
-        ${createCard('28/11', 'Segunda',
+        createCard('28/11', 'Segunda',
         createGame('south-korea-flag', '10:00', 'ghana-flag') +
         createGame('brazil-flag', '13:00', 'switzerland-flag') +
-        createGame('portugal-flag', '16:00', 'uruguay-flag')
+        createGame('portugal-flag', '16:00', 'uruguay-flag')) +
         
-        )}
 
-        ${createCard('02/12', 'Sexta',
+        createCard('02/12', 'Sexta',
         createGame('ghana-flag', '12:00', 'uruguay-flag') +
-        createGame('brazil-flag', '16:00', 'cameroon-flag')
+        createGame('brazil-flag', '16:00', 'cameroon-flag'))
         
-        )}
-    </main> `
